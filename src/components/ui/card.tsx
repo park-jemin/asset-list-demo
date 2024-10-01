b09bafc55c5feebe51d5c3b0dcaf45f27e2fda8f
Jemin Park <jemin.park@outlook.com>
2024-09-30 22:46:04 -0400

    feat: create a basic login page

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,68 @@
+import * as React from 'react';
+
+import { cn } from '@/lib/utils';
+
+// todo: make this actually polymorphic
+const Card = React.forwardRef<
+  HTMLDivElement | HTMLFormElement,
+  React.HTMLAttributes<HTMLDivElement | HTMLFormElement> & { as?: React.ElementType }
+>(({ className, as, ...props }, ref) => {
+  const Component = as || 'div';
+  return (
+    <Component
+      ref={ref}
+      className={cn('rounded-lg border bg-card text-card-foreground shadow-sm', className)}
+      {...props}
+      onSubmit={props.onSubmit}
+    />
+  );
+});
+Card.displayName = 'Card';
+
+const CardHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn('flex flex-col space-y-1.5 p-6', className)} {...props} />
+  )
+);
+CardHeader.displayName = 'CardHeader';
+
+const CardTitle = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLHeadingElement> & {
+    as?: Extract<React.ElementType, 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'>;
+  }
+>(({ className, as, ...props }, ref) => {
+  const Component = as || 'h3';
+  return (
+    <Component
+      ref={ref}
+      className={cn('font-semibold text-2xl leading-none tracking-tight', className)}
+      {...props}
+    />
+  );
+});
+CardTitle.displayName = 'CardTitle';
+
+const CardDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <p ref={ref} className={cn('text-muted-foreground text-sm', className)} {...props} />
+));
+CardDescription.displayName = 'CardDescription';
+
+const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn('p-6 pt-0', className)} {...props} />
+  )
+);
+CardContent.displayName = 'CardContent';
+
+const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn('flex items-center p-6 pt-0', className)} {...props} />
+  )
+);
+CardFooter.displayName = 'CardFooter';
+
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent };
---
+++ src/pages/login.page.tsx
@@ -0,0 +1,54 @@
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+import type { FormEventHandler } from 'react';
+
+export default function LoginPage() {
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const email = (form.elements.namedItem('email') as HTMLInputElement).value;
+    const password = (form.elements.namedItem('password') as HTMLInputElement).value;
+
+    // Ignore failing cases for now, don't need to be exhaustive
+    console.log({ email, password });
+  };
+
+  return (
+    <main className="relative flex min-h-screen min-w-80 flex-col bg-background">
+      <div className="flex h-screen w-full flex-col items-center justify-center gap-16 bg-background px-4">
+        <Card as="form" onSubmit={handleSubmit} className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle as="h1" className="text-2xl">
+              Login
+            </CardTitle>
+            <CardDescription>Enter your email below to login to your account.</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" placeholder="hello@example.com" required />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" placeholder="*********" required />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">Sign in</Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </main>
+  );
+}
